0e1f4bec8b18bd4cce37e846cbc8c83fd2476823
HugoWEI <74794456+HugoWEI@users.noreply.github.com>
2020-11-20 21:47:33 +0100

    Update flappy.js

--- webapp-flappy-master/js/flappy.js
+++ webapp-flappy-master/js/flappy.js
@@ -41,7 +41,7 @@ var game = new Phaser.Game(width, height, Phaser.AUTO, 'game', stateActions);
 function preload() {
 
        //  game.load.image("playerImg", "../assets/flag.jpg", 37, 45, 18);
-        game.load.spritesheet("playerImg", "../assets/patricemummy.png", 37, 45, 18);
+        game.load.spritesheet("playerImg", "../assets/patricemummy.png", 100, 45, 18);
          // game.load.spritesheet("playerImg", "../assets/otis.jpeg", 37, 45, 18);
         //  game.load.spritesheet("playerImg", "../assets/otis.jpeg");
           game.load.image("playerImg1", "../assets/jamesBond.gif");
